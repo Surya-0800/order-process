2bff5d912ff5d8c8f6a538165d8c02b734db3b72
SuryaReddy97 <surya@jagdushah.com>
2025-10-20 22:17:05 +0530

    Removed debug condition for urls

--- ordercycle/urls.py
+++ ordercycle/urls.py
@@ -165,6 +165,4 @@ urlpatterns = [
 
 ]
 
-# Add media URL patterns for development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
